102ead2f9884298f73d0859dafbab84f5157fcef
abdullahk99 <abdullah.khokhar@mail.utoronto.ca>
2018-12-28 15:49:09 -0500

    new view

--- main-page.js
+++ main-page.js
@@ -1,10 +1,9 @@
 (function(){
   var menu = document.querySelector('ul');
-  var menulink = document.querySelector('img');
+  var menulink = document.getElementById("menu");
 
   menulink.addEventListener('click', function(e){
     menu.classList.toggle('active');
     e.preventDefault();
   });
-
 })();
