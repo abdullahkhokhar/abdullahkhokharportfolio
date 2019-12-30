861afb9e2642bb0fa287fda2a34c83336843c723
Abdullah Khokhar <abdullah.khokhar@mail.utoronto.ca>
2019-12-30 18:56:28 -0500

    website worked on over the course of 4 months, changed and developed

--- main-page.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  var menu = document.querySelector('ul');
-  var menulink = document.getElementById("menu");
-
-  menulink.addEventListener('click', function(e){
-    menu.classList.toggle('active');
-    e.preventDefault();
-  });
-})();
